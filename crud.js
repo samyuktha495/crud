54a71ae2a11cb42c5cbd4c9e5b803970f80cde61
samyuktha495 <antagirisamyuktha@gmail.com>
2024-11-17 12:19:54 +0530

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,51 @@
+function addTask() {
+
+    let inputData = document.getElementById("enterData");
+    let inputDataValue = inputData.value.trim()
+
+    let allTasksContainer = document.querySelector("#tasks")
+
+    let liTag = document.createElement("li");
+
+    liTag.innerHTML += `
+    ${inputDataValue}
+     <button class="edit">Edit</button>
+     <button class="delete">Delete</button> `
+
+    liTag.style.listStyleType = "none"
+
+    
+  
+    allTasksContainer.appendChild(liTag)
+
+    let editBtn = liTag.querySelector(".edit")
+    editBtn.style.backgroundColor = "blue"
+
+    let dlteBtn = liTag.querySelector(".delete")
+    dlteBtn.style.backgroundColor = "red"
+    
+    inputData.value = ""
+
+dlteBtn.addEventListener("click",()=>{
+    deleteTask(liTag)
+    console.log("df is called")
+})
+
+editBtn.addEventListener("click",()=>{
+    EditTask(liTag)
+ })
+}
+ function deleteTask(liTag){
+    let a=confirm("are you wanna delete the task")
+    if(a){
+        liTag.remove()
+    }
+ }
+
+
+ function EditTask(item){
+    let x=prompt("enter the data",item.firstChild.textContent)
+    if(x!==null && x.trim()!==""){
+    item.firstChild.textContent=x;
+ }
+ }
